c3e3f6685e843505fc500d8d40509a7a1f317f7a
Hoon Lee <jiroh618@gmail.com>
2022-12-30 15:47:55 +0900

    feat make apps

---
+++ backend/finance/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from ..summary.models import TbName
+
+# Create your models here.
+class TbOHLCV(models.Model):
+	id = models.AutoField(db_column='ID', primary_key=True)
+	code = models.ForeignKey(TbName, models.DO_NOTHING, db_column='code')
+	date = models.DateTimeField(db_column='date', blank=True, null=True)
+	compare_price = models.FloatField(db_column='compare_price', blank=True, null=True)
+	open_price = models.IntegerField(db_column='open_price', blank=True, null=True)
+	high_price = models.IntegerField(db_column='high_price', blank=True, null=True)	
+	low_price = models.IntegerField(db_column='low_price', blank=True, null=True)	
+	close_price = models.IntegerField(db_column='close_price', blank=True, null=True)	
+	trading_volume = models.IntegerField(db_column='trading_volume', blank=True, null=True)
+	trading_value = models.IntegerField(db_column='trading_value', blank=True, null=True)
+
+class TbFundamental(models.Model):
+	id = models.AutoField(db_column='ID', primary_key=True)
+	code = models.ForeignKey(TbName, models.DO_NOTHING, db_column='code')
+	date = models.DateTimeField(db_column='date', blank=True, null=True)
+	BPS = models.FloatField(db_column='BPS', blank=True, null=True)
+	PER = models.FloatField(db_column='PER', blank=True, null=True)
+	PBR = models.FloatField(db_column='PBR', blank=True, null=True)
+	EPS = models.FloatField(db_column='EPS', blank=True, null=True)
+	DIV = models.FloatField(db_column='DIV', blank=True, null=True)
+	DPS = models.FloatField(db_column='DPS', blank=True, null=True)
+
+
+class TbProfit(models.Model):
+	id = models.AutoField(db_column='ID', primary_key=True)
+	code = models.ForeignKey(TbName, models.DO_NOTHING, db_column='code')
+	date = models.DateTimeField(db_column='date', blank=True, null=True)
+	profit = models.IntegerField(db_column='profit', blank=True, null=True)
+
+
+class TbShort(models.Model):
+	id = models.AutoField(db_column='ID', primary_key=True)
+	code = models.ForeignKey(TbName, models.DO_NOTHING, db_column='code')
+	date = models.DateTimeField(db_column='date', blank=True, null=True)
+	balance = models.IntegerField(db_column='balance', blank=True, null=True)
+	volume = models.IntegerField(db_column='volume', blank=True, null=True)
+	value = models.IntegerField(db_column='value', blank=True, null=True)
+	market_cap = models.IntegerField(db_column='market_cap', blank=True, null=True)
+	weight = models.IntegerField(db_column='weight', blank=True, null=True)
+
+
+class TbTradingValue(models.Model):
+	id = models.AutoField(db_column='ID', primary_key=True)
+	code = models.ForeignKey(TbName, models.DO_NOTHING, db_column='code')
+	date = models.DateTimeField(db_column='date', blank=True, null=True)
+	institution = models.IntegerField(db_column='institution', blank=True, null=True)
+	retail_investor = models.IntegerField(db_column='retail_investor', blank=True, null=True)
+	etc_corporation = models.IntegerField(db_column='etc_corporation', blank=True, null=True)
+	foreigner = models.IntegerField(db_column='foreigner', blank=True, null=True)
+
+
+class TbTradingVolume(models.Model):
+	id = models.AutoField(db_column='ID', primary_key=True)
+	code = models.ForeignKey(TbName, models.DO_NOTHING, db_column='code')
+	date = models.DateTimeField(db_column='date', blank=True, null=True)
+	institution = models.IntegerField(db_column='institution', blank=True, null=True)
+	retail_investor = models.IntegerField(db_column='retail_investor', blank=True, null=True)
+	etc_corporation = models.IntegerField(db_column='etc_corporation', blank=True, null=True)
+	foreigner = models.IntegerField(db_column='foreigner', blank=True, null=True)
+
+
+class TbForeignerInvestment(models.Model):
+	id = models.AutoField(db_column='ID', primary_key=True)
+	code = models.ForeignKey(TbName, models.DO_NOTHING, db_column='code')
+	volume = models.IntegerField(db_column='volume', blank=True, null=True)
+	holding_volume = models.IntegerField(db_column='holding_volume', blank=True, null=True)
+	shareholding = models.IntegerField(db_column='shareholding', blank=True, null=True)
+	limit_volume = models.IntegerField(db_column='limit_volume', blank=True, null=True)
+	exhaustion_rate = models.IntegerField(db_column='exhaustion_rate', blank=True, null=True)
--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_docker.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "white_hazelnut.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
