5652220f0b73f958192b7ece0890bb68ed793c0c
Hoon Lee <jiroh618@gmail.com>
2023-01-04 14:22:16 +0900

    feat upload py_db

---
+++ Database/py_db/rdb.py
@@ -0,0 +1,191 @@
+from __future__ import annotations
+
+import pymysql
+import traceback
+
+class MariaDB:
+
+    def __init__(self, db_config:dict, cursor_type="tuple") -> None:
+        '''
+        생성자 메서드
+        인스턴스 생성 시 db_config를 전달받아 DB에 연결합니다.
+        
+        **db_config**
+            host=데이터베이스 서버 주소
+            port=포트번호
+            user=사용자 이름
+            password=사용자 암호
+            database=데이터베이스 이름
+            charset=문자 인코딩
+        '''
+
+        db_config['port'] = int(db_config.get('port', '3306'))
+        self.DB = pymysql.connect(**db_config)
+
+        if cursor_type == 'dict':
+            self.cursor_type = pymysql.cursors.DictCursor
+        else:
+            self.cursor_type = None
+            
+        return
+
+    def __del__(self):
+        '''
+        인스턴스 소멸 시 DB 연결을 해제합니다.
+        '''
+        self.DB.close()
+        return
+    
+    def custom_select(self, sql_qr:str, only_one=False) -> tuple:
+        with self.DB.cursor() as cur:
+            cur.execute(sql_qr)
+            return cur.fetchall() if only_one else cur.fetchone()
+
+    def select(self, column_qry:str, table:str, limit=None, offset=None, order_by=None, where_condition:list[tuple]=None) -> tuple:
+        """
+        Select
+        예시) 
+        column_qry = "*"
+        column_qry = "id, name, email"
+        table = "Students"
+        """
+        sql_qr = "SELECT {0} FROM {1}".format(column_qry, table)
+        if limit:
+            sql_qr += ' LIMIT {}'.format(limit)
+        if offset:
+            sql_qr += ' OFFSET {}'.format(offset)
+        if order_by:
+            sql_qr += ' ORDER BY {}'.format(order_by)
+        if where_condition:
+            for i, (col, eq, val) in enumerate(where_condition):
+                is_equal = '=' if eq else '!='
+                is_multiple = ' AND' if i > 1 else ' WHERE'
+                sql_qr += f'{is_multiple} {col}{is_equal}{val}'
+
+
+        with self.DB.cursor() as cur:
+            cur.execute(sql_qr)
+            return cur.fetchall()
+
+    def insert(self, table:str, columns: str, value: tuple) -> bool:
+        """
+        Insert Data
+        예시)
+        table = "Students"
+        columns = "name, email, phone"
+        values = ('이름', '이메일', '번호')
+        """
+
+        sql_qr = f"INSERT INTO {table}({columns}) " \
+                  "VALUES (" +','.join(["%s"]*len(value)) +")"
+        # args = values
+        
+        try:
+            with self.DB.cursor() as cur:
+                cur.execute(sql_qr, value)
+                self.DB.commit()
+            return cur.lastrowid
+        except:
+            traceback.print_exc()
+            return False
+    
+    # values는 list 형식으로 넣었음, args로 함
+    def insert_many(self, table:str, columns: str, values: list) -> bool:
+        """
+        Insert Many Datas
+        예시)
+        table = "Students"
+        columns = "name, email, phone"
+        values = [
+            ('이름', '이메일', '번호'),
+            ...
+        ]
+        """
+        sql = f"INSERT INTO {table}({columns}) " \
+                  "VALUES ("  + ','.join(["%s"]*len(values[0])) + ");"
+        try:
+            with self.DB.cursor() as cur:
+                cur.executemany(sql, values)
+                self.DB.commit()
+            return True
+        except:
+            traceback.print_exc()
+            return False
+
+
+
+    def update(self, table:str, set_column:str, set_value, where_column:str, where_value) -> bool:
+        """
+        Update
+        예시)
+        table = "Students"
+        set_column = "name"
+        set_value = "jason"
+        where_column = "id"
+        where_value = "1"
+        """
+        sql = "UPDATE {0}" \
+            "SET {1}={2}" \
+            "WHERE {3}={4};".format(table, set_column, set_value, where_column, where_value)
+        try:
+            with self.DB.cursor() as cur:
+                cur.execute(sql)
+                self.DB.commit()
+            return True
+        except:
+            return False
+    
+    def delete(self, table:str, where_column:str, where_value) -> bool:
+        """
+        Delete
+        예시)
+        table = "Students"
+        where_column = "id"
+        where_value = "1"
+        """
+        sql = """DELETE FROM {0} WHERE {1}={2}""".format(table, where_column, where_value)
+        try:
+            with self.DB.cursor() as cur:
+                cur.execute(sql)
+                self.DB.commit()
+            return True
+        except:
+            return False
+            
+    @staticmethod
+    def read_config(config_dir: str) -> dict:
+        """
+        데이터베이스 config 파일 읽어오기
+        config_dir = 'db_config 파일 경로'
+
+        **db_config**
+            host=데이터베이스 서버 주소
+            port=포트번호
+            user=사용자 이름
+            password=사용자 암호
+            database=데이터베이스 이름
+            charset=문자 인코딩
+        """
+        db_config = {}
+        with open(config_dir, 'r') as f:
+            for l in f.readlines():
+                key, value = l.rstrip().split('=')
+                if key == 'port':
+                    db_config[key] = int(value)
+                else:
+                    db_config[key] = value
+        return db_config
+    
+    @staticmethod
+    def make_columns(data: dict) -> str:
+        """
+        dictionary 객체에서 키를 칼럼 문자열로 변경
+        """
+        return ','.join(data.keys())
+    
+    @staticmethod
+    def make_values(data: dict) -> tuple:
+        """
+        dictionary 객체에서 value를 tuple로 변경
+        """
+        return tuple(data.values())
---
+++ Database/py_db/stock_db.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import pymysql
+from rdb import MariaDB
+
+
+class stock_db:
+    '''
+    final db src 
+    '''
+    def __init__(self, db:MariaDB) -> None:
+        self.db = db
+        
+    def insert_news(self, df:pd.DataFrame) -> bool:
+        '''
+        종목 관련 데이터 프레임을 통해 데이터베이스에 종목 정보를 삽입합니다. 
+        '''
+        # 데이터프레임 drop_duplicates()해서 df에 적용
+        # df.drop_duplicates(subset='URL',inplace=True) # 중복제거
+        # df.fillna('', inplace=True) # TypeError: replace() argument 1 must be str, not float
+        print('### 종목관련 (code, name, market)에 대한 정보가 들어 왔습니다.')
+        columns = df.columns
+        df.reset_index(inplace=True)
+        df = df[columns]
+
+        return self.db.insert_many('tb_name', ','.join(df.columns), df.values.tolist()) # 성공, 실패
+
+    # # 둘 중 하나를 사용
+    # # def select_news(self, url_list:list) -> dict:
+    # def select_news(self, df:pd.DataFrame) -> dict:
+    #     '''
+    #     뉴스 데이터를 검색하여 ID, URL값을 반환합니다.
+    #     '''
+    #     # select query 작성
+    #     # 가져올 값 - id, url
+    #     # WHERE - IN
+    #     url_list = df['URL'].tolist()
+    #     sql_query = '''SELECT ID, URL
+    #     FROM tb_news
+    #     WHERE URL IN ("{}")
+    #     '''.format('", "'.join(url_list))
+    #     # self.db.DB.
+    #     with self.db.DB.cursor() as cur:
+    #         cur.execute(sql_query)
+    #         datas = cur.fetchall()
+
+    #     news_dict = {}
+    #     for news_id, url in datas:
+    #         news_dict[url] = news_id
+
+    #     return news_dict # {url: id} dictionary
+    
+    # def insert_user(self, df:pd.DataFrame) -> bool:
+    #     '''
+    #     유저정보를 삽입합니다.
+    #     '''
+    #     df = df.drop_duplicates(subset='UserID',keep='last')
+        
+    #     error_list=[]
+    #     try:
+    #         df['DomainID'] = df['URL'].apply(lambda x : 1 if 'naver' in x else 2)
+    #         df = df[['DomainID', 'UserID','UserName']]
+    #         df.fillna('', inplace=True)
+    #         columns = df.columns
+    #         value=list(df.itertuples(index=False, name=None))
+    #         sql_qr = f"INSERT INTO tb_user({','.join(columns)}) " \
+    #             "VALUES (" +','.join(["%s"]*len(value[0])) + ") on DUPLICATE KEY UPDATE UserID=UserID;"
+            
+    #         with self.db.DB.cursor() as cur:
+    #             cur.executemany(sql_qr, value)
+    #             datas = self.db.DB.commit()
+
+    #         return True
+
+    #     except Exception as e:
+    #         import traceback
+    #         traceback.print_exc()
+    #         # print(e)
+    #         return False
+
+    
+    # def select_user(self, df:pd.DataFrame) -> dict:
+    #     '''
+    #     모든 유저 정보를 가져옵니다.
+    #     {
+    #         'UserID': { ID: , DomainID: , UserName: },
+    #     }
+    #     '''
+
+    #     UserID_list = df['UserID'].tolist()
+    #     sql_query = '''SELECT UserID, ID, DomainID, UserName FROM tb_user
+    #     WHERE UserID IN ("{}")
+    #     '''.format('","'.join(map(str, UserID_list)))
+
+    #     with self.db.DB.cursor() as cur:
+    #         cur.execute(sql_query)
+    #         datas = cur.fetchall()
+
+    #     userID_dict = {}
+    #     for UserID, ID, DomainID, UserName in datas:
+    #         userID_dict[UserID] = {'ID':ID, 'DomainID':DomainID, 'UserName':UserName}
+        
+    #     return userID_dict 
+
+    # def insert_comments(self, df:pd.DataFrame, news_dict, user_json) -> bool:
+    #     '''
+    #     댓글 데이터를 삽입합니다.
+    #     df : comment_df
+    #     '''
+
+    #     # WritedAt 포맷 변경 (%Y-%m-%d %H:%M:%S)
+    #     df['WritedAt'] = pd.to_datetime(df['WritedAt']) if 'WritedAt' in df.columns else ''
+    #     df['check'] = df['URL'].apply(lambda x: x in news_dict)
+    #     df = df[df['check'] == True]
+    #     df.fillna('', inplace=True)
+    #     df.reset_index(inplace=True)
+    #     df['NewsID'] = df['URL'].apply(lambda x : news_dict[x] if x in news_dict else None)
+    #     df.dropna(inplace=True)
+    #     df.reset_index(inplace=True)
+    #     df['UserID'] = df['UserID'].apply(lambda x: user_json[str(x)]['ID'])
+
+    #     #db에 적재할 최종 데이터프레임
+    #     df=df[['NewsID','UserID','WritedAt','Content']]
+    #     value=list(df.itertuples(index=False, name=None))
+
+    #     try:
+    #         sql = '''insert into tb_comment(NewsID,UserID,WritedAt,Content) 
+    #         values(%s,%s,%s,%s);'''  
+
+    #         with self.db.DB.cursor() as cur:
+    #             cur.executemany(sql, value)
+    #             self.db.DB.commit()
+
+    #     except Exception as e:
+    #         return print('db insert 실패',e)
+
+
+        
+        
