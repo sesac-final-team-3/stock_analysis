4e36b05eba5421fa1963216fa462e3a748f9da33
sssunyoung <sun517young@gmail.com>
2023-01-10 16:38:47 +0900

    modify django model

--- backend/news/models.py
+++ backend/news/models.py
@@ -8,10 +8,9 @@ class TbNews(models.Model):
     code = models.ForeignKey(TbName, models.DO_NOTHING, db_column='code')
     press = models.CharField(db_column='press', max_length=16, blank=True, null=True)
     url = models.TextField(db_column='url', blank=True, null=True)
-    title = models.CharField(db_column='title', max_length=64, blank=True, null=True)
+    title = models.TextField(db_column='title', blank=True, null=True)
     content = models.TextField(db_column='content', blank=True, null=True)
     photourl = models.TextField(db_column='photourl', blank=True, null=True)
-    graph = models.TextField(db_column='graph', blank=True, null=True)
     updated_date = models.DateTimeField(db_column='updated_date', blank=True, null=True)
 
     class Meta:
