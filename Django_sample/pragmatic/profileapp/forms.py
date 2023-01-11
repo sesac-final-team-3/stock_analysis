9180a5b7f61c0ca940bde54e6e9681d616478d07
jjongseon <karls08@naver.com>
2023-01-11 17:40:46 +0900

    remove test frontend

--- Django_sample/pragmatic/profileapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from profileapp.models import Profile
-
-
-class ProfileCreationForm(ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['image', 'nickname', 'message']
