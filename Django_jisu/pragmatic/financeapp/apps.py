0922d213e5653f3a48d7f4d617c2ae2dbca49b2c
jjongseon <karls08@naver.com>
2023-01-18 13:17:04 +0900

    feat news chart

--- Django_jisu/pragmatic/financeapp/apps.py
+++ Django_jisu/pragmatic/financeapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FinanaceappConfig(AppConfig):
+class FinanceappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'finanaceapp'
+    name = 'financeapp'
--- Django_jisu/pragmatic/pragmatic/settings.py
+++ Django_jisu/pragmatic/pragmatic/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'newsapp',
     'articleapp',
     'searchapp',
-    'finanaceapp',
+    'financeapp',
 ]
 
 MIDDLEWARE = [
